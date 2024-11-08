9d9765155b69edeabb42a5135be0e26c1e0bebc3
Lata1989 <pabloalejandrodelaiglesia@gmail.com>
2024-11-07 22:34:09 -0300

    listo

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,28 +1,30 @@
 import React from 'react';
-// import { ProjectCard } from '../components/ProjectCard'; // Asumiendo que creaste este componente para mostrar proyectos
+import { ProjectCard } from '../components/ProjectCard'; // Asumiendo que creaste este componente para mostrar proyectos
 import '../styles/projects.css';
 import nodirenada from '../assets/Johnny_Tightlips.png';
 
-// const projectsData = [
-//     {
-//         name: 'Gestor de Colas',
-//         description: 'Sistema de gestión de colas para comercios.',
-//     },
-//     {
-//         name: 'Generador de Respuestas con Gemini AI',
-//         description: 'Sistema de generación automática de respuestas usando Google Generative AI.',
-//     }
-// ];
+const projectsData = [
+    {
+        name: 'Lista de tareas',
+        description: `Sistema de lista de tareas en Angular con local Storage.`,
+        url: 'https://app-lista-tareas-angular-localstorage.vercel.app/'
+    },
+    {
+        name: 'CRUD App React',
+        description: 'Sistema de CRUD en React con autenticación via Auth0.',
+        url: 'https://login-logout-crud-react-p3-frontend.vercel.app/'
+    }
+];
 
 export const Projects = () => {
     return (
         <div className="projects">
             <h1>Mis Proyectos</h1>
             <div className="projects-list">
-                <img className='johny' src={nodirenada} alt="Aca debería haber proyectos." />
-                {/* {projectsData.map((project, index) => (
+                {/* <img className='johny' src={nodirenada} alt="Aca debería haber proyectos." /> */}
+                {projectsData.map((project, index) => (
                     <ProjectCard key={index} project={project} />
-                ))} */}
+                ))}
             </div>
         </div>
     );
